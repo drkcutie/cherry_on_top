d1aa8bfc2cb63c8e42653abec9a3bf1d15a2bbfd
drkcutie <derrick.binangbang@cit.edu>
2025-02-25 19:00:53 +0800

    added bottom navigations

--- resiklo/app/(tabs)/_layout.tsx
+++ resiklo/app/(tabs)/_layout.tsx
@@ -1,60 +1,74 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import {Link, Tabs} from 'expo-router';
+import {Pressable} from 'react-native';
 
 import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import {useColorScheme} from '@/components/useColorScheme';
+import {useClientOnlyValue} from '@/components/useClientOnlyValue';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import TabTwoScreen from "@/app/(tabs)/two";
+import {House, Smartphone, Trophy, User, UserRound} from "lucide-react-native";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const Tab = createBottomTabNavigator();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true)
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tab.Navigator screenOptions={{tabBarActiveTintColor: 'green', tabBarInactiveTintColor: 'white'}}>
+            <Tab.Screen
+                name="Feed"
+                component={TabTwoScreen}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({focused}) => (
+                    <House color={focused ? 'green' : 'white'} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Socials"
+                component={TabTwoScreen}
+                options={{
+                    tabBarLabel: 'Socials',
+                    tabBarIcon: ({focused}) => (
+                    <UserRound color={focused ? 'green' : 'white'} />
+                    ),
+                }}
+
+            />
+            <Tab.Screen
+                name="Scan"
+                component={TabTwoScreen}
+                options={{
+                    tabBarLabel: 'Scan',
+                    tabBarIcon: ({focused}) => (
+                    <Smartphone color={focused ? 'green' : 'white'} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Rewards"
+                component={TabTwoScreen}
+                options={{
+                    tabBarLabel: 'Rewards',
+                    tabBarIcon: ({focused}) => (
+                    <Trophy color={focused ? 'green' : 'white'} />
+
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Account"
+                component={TabTwoScreen}
+                options={{
+                    tabBarLabel: 'Account',
+                    tabBarIcon: ({focused}) => (
+                    <User color={focused ? 'green' : 'white'} />
+            ),
+                }}
+            />
+        </Tab.Navigator>
+    );
 }
