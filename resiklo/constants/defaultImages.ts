4e184741e9e86df1d09e572970e346cef69b0062
leeroi02 <felicityjcksn@gmail.com>
2025-03-05 01:29:01 +0800

    added save activity and expo image picker

--- resiklo/constants/defaultImages.ts
+++ resiklo/constants/defaultImages.ts
@@ -1,6 +1,7 @@
 const defaultImages = {
   derik : require("../assets/images/derik.png"),
-  globe : require ("../assets/images/globe.png")
+  globe : require ("../assets/images/globe.png"),
+  totalPoints: require("../assets/images/total-points.png"),
 }
 
 export default defaultImages;
