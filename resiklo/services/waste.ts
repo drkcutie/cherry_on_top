222c6a00ea3f26f0f4de623cd33ba22bf4db7115
Philippe Andrei <philippeandreid@gmail.com>
2025-03-05 06:07:06 +0800

    added waste

--- resiklo/services/waste.ts
+++ resiklo/services/waste.ts
@@ -1,9 +1,16 @@
 import { supabase } from '@/lib/supabase';
 
-export const  getWaste = async () => {
-    // let { data: waste, error } = await supabase
-    // .from('waste')
-    // .select('*')
-    // return waste
-    return 
+export const  getWaste = async (image_id: number) => {
+    
+    const { data: waste, error } = await supabase
+        .from('waste')
+        .select('*')
+        .eq('image_id', image_id)
+
+    if (error) {
+        console.log(error)
+        return []
+    }
+    
+    return waste
 }
